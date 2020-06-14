8bc695a9eb7f80375d1af657e6071abbc9a7338b
Vladimir Scherbina <vns.scherbina@gmail.com>
2020-06-15 01:29:28 +0300

    Add standalone mode SSL support

--- certificates/generate.sh
+++ certificates/generate.sh
@@ -1,5 +1,5 @@
 cd "$(dirname "$0")"
-openssl genrsa -out key.pem 2048
-openssl req -new -out self.pem -key key.pem -subj '/CN=localhost'
-openssl req -text -noout -in self.pem
-openssl x509 -req -days 1024 -in self.pem -signkey key.pem -out cert.pem -extfile generate.ext
+openssl genrsa -out $1.server.key 2048
+openssl req -new -out $1.server.csr -key $1.server.key -subj "/CN=$1"
+openssl req -text -noout -in $1.server.csr
+openssl x509 -req -days 1024 -in $1.server.csr -signkey $1.server.key -out $1.server.crt -extfile generate.ext
