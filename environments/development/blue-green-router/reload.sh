01b3755c537384f68a5e99b821c5d50194f96036
Vladimir Scherbina <vns.scherbina@gmail.com>
2020-06-18 02:34:13 +0300

    Refactor networking to expose only nginx routers

--- environments/development/blue-green-router/reload.sh
+++ environments/development/blue-green-router/reload.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 cp /usr/src/app/router.conf /etc/nginx/nginx.template
-export $(echo "BLUE_GREEN_HOST=$(ip route show | awk '/default/ {print $3}')" | xargs)
 envsubst < /etc/nginx/nginx.template > /etc/nginx/nginx.conf
 if (nginx -t); then nginx -s reload; fi
